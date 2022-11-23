5d524469365df4aaaf68b06998d0ffb44630a55b
RubenPari <rubenpari.developer@gmail.com>
2022-11-23 20:22:25 +0100

    added inpagination on song summary endp

--- src/controllers/song/song.go
+++ src/controllers/song/song.go
@@ -2,45 +2,78 @@ package song
 
 import (
 	"context"
+	"log"
+
 	"github.com/RubenPari/clear-songs/src/models"
 	authMO "github.com/RubenPari/clear-songs/src/modules/auth"
 	"github.com/RubenPari/clear-songs/src/modules/utils"
 	"github.com/gofiber/fiber/v2"
 	spotifyAPI "github.com/zmb3/spotify/v2"
-	"log"
 )
 
 // Summary get a list of artistLibrarySummary objects
+// TODO: fix
 func Summary(c *fiber.Ctx) error {
 	spotifyClient := authMO.SpotifyClient
 	ctx := context.Background()
 
-	songs, err := spotifyClient.CurrentUsersTracks(ctx, spotifyAPI.Limit(10))
+	// call to spotify api n times based on the offset
+	var limit = 50
+	var offset = 0 // NOTE: offset is excluded
+	// NOTE: gli elementi selezionati da n a m range
+	// non sono salvati in tale ordine
 
-	if err != nil {
-		log.Default().Printf("couldn't get songs: %v", err)
+	var errGetSavedTracks error
+
+	songs := make([]spotifyAPI.SavedTrack, 0)
+
+	for {
+		tracksPage, err := spotifyClient.CurrentUsersTracks(ctx, spotifyAPI.Limit(limit), spotifyAPI.Offset(offset))
+
+		if err != nil {
+			errGetSavedTracks = err
+			break
+		}
+
+		songs = append(songs, tracksPage.Tracks...)
+
+		if len(tracksPage.Tracks) < limit {
+			break
+		}
+
+		offset += limit
+	}
+
+	if errGetSavedTracks != nil {
+		log.Default().Printf("couldn't get songs: %v", errGetSavedTracks)
 		_ = c.SendStatus(fiber.StatusInternalServerError)
 		return c.JSON(fiber.Map{
 			"status":  "error",
 			"message": "couldn't get songs",
 		})
 	}
-	artistsSummary := make([]models.ArtistLibrarySummary, 0)
-	var artistsFounded [50]string
 
-	for _, song := range songs.Tracks {
-		founded, index := utils.ArrayContains(artistsFounded, string(song.Artists[0].ID))
+	artistsSummary := make([]models.ArtistLibrarySummary, 0)
+	var artistsFounded [100]string
+
+	for _, song := range songs {
+		founded := utils.ArrayArtistFoundedContains(&artistsFounded, string(song.Artists[0].ID))
 
 		if !founded {
 			artistsSummary = append(artistsSummary, models.ArtistLibrarySummary{
-				Id:   string(song.Artists[0].ID),
-				Name: song.Artists[0].Name,
-				Num:  1,
+				Id:       string(song.Artists[0].ID),
+				Name:     song.Artists[0].Name,
+				NumSongs: 1,
 			})
 
-			utils.AppendArray(&artistsFounded, string(song.Artists[0].ID))
+			utils.
+				AppendArray(&artistsFounded, string(song.Artists[0].ID))
 		} else {
-			artistsSummary[index].Num++
+			founded, index := utils.ArrayArtistSummaryContains(&artistsSummary, string(song.Artists[0].ID))
+
+			if founded {
+				artistsSummary[index].NumSongs++
+			}
 		}
 	}
 
