4ee86db5fd4d012f8f8d1cb03cfc00795f014793
RubenPari <rubenpari.developer@gmail.com>
2023-01-09 14:15:34 +0100

    mapped some funcs

--- src/modules/utils/utils.go
+++ src/modules/utils/utils.go
@@ -1,7 +1,11 @@
 package utils
 
 import (
+	"context"
+	"log"
+
 	"github.com/RubenPari/clear-songs/src/models"
+	authMO "github.com/RubenPari/clear-songs/src/modules/auth"
 	spotifyAPI "github.com/zmb3/spotify/v2"
 )
 
@@ -50,4 +54,69 @@ func ArrayArtistSummaryContains(array *[]models.ArtistLibrarySummary, value stri
 	}
 
 	return false, 0
+}
+
+// GetTracksUser get all tracks saved by the user
+func GetTracksUser() ([]spotifyAPI.SavedTrack, error) {
+	spotifyClient := authMO.SpotifyClient
+	ctx := context.Background()
+
+	// call to spotify api n times based on the offset
+	var limit = 50
+	var offset = 0 // NOTE: offset is excluded
+	// NOTE: gli elementi selezionati da n a m range
+	// non sono salvati in tale ordine
+
+	var errGetSavedTracks error
+
+	songs := make([]spotifyAPI.SavedTrack, 0)
+
+	for {
+		tracksPage, err := spotifyClient.CurrentUsersTracks(ctx, spotifyAPI.Limit(limit), spotifyAPI.Offset(offset))
+
+		if err != nil {
+			errGetSavedTracks = err
+			break
+		}
+
+		songs = append(songs, tracksPage.Tracks...)
+
+		if len(tracksPage.Tracks) < limit {
+			break
+		}
+
+		offset += limit
+	}
+
+	if errGetSavedTracks != nil {
+		log.Default().Printf("couldn't get songs: %v", errGetSavedTracks)
+		return nil, errGetSavedTracks
+	} else {
+		return songs, nil
+	}
+}
+
+func RemoveUserTracks(tracks []spotifyAPI.ID) error {
+	spotifyClient := authMO.SpotifyClient
+	ctx := context.Background()
+
+	var errRemoveTracks error = nil
+
+	for start := 0; start < len(tracks); start += 50 {
+		end := start + 50
+
+		if end > len(tracks) {
+			end = len(tracks)
+		}
+
+		err := spotifyClient.RemoveTracksFromLibrary(ctx, tracks[start:end]...)
+
+		if errRemoveTracks != nil {
+			log.Default().Printf("couldn't remove songs: %v", errRemoveTracks)
+			errRemoveTracks = err
+			break
+		}
+	}
+
+	return errRemoveTracks
 }
