d196b85c34be907e6ebe421122ab3054b6e91546
RubenPari <rubenpari.developer@gmail.com>
2023-04-13 16:48:11 +0200

    fixed GetArtistsSummary func

--- src/lib/artist/artist.go
+++ src/lib/artist/artist.go
@@ -1,16 +1,28 @@
 package artist
 
-import spotifyAPI "github.com/zmb3/spotify"
+import (
+	"github.com/RubenPari/clear-songs/src/models"
+	spotifyAPI "github.com/zmb3/spotify"
+)
 
 // GetArtistsSummary returns a
 // map with the number of tracks
 // of each artist
-func GetArtistsSummary(tracks []spotifyAPI.SavedTrack) map[string]int {
+func GetArtistsSummary(tracks []spotifyAPI.SavedTrack) []models.ArtistSummary {
 	var artistSummary = make(map[string]int)
 
 	for _, track := range tracks {
 		artistSummary[track.Artists[0].Name]++
 	}
 
-	return artistSummary
+	var artistSummaryArray []models.ArtistSummary
+
+	for artist, count := range artistSummary {
+		artistSummaryArray = append(artistSummaryArray, models.ArtistSummary{
+			Name:  artist,
+			Count: count,
+		})
+	}
+
+	return artistSummaryArray
 }
