9870546a24a65fb234bbef62fa57ca87232e77f5
RubenPari <rubenpari.developer@gmail.com>
2023-08-27 19:29:14 +0200

    edited run.ps1

---
+++ run.ps1
@@ -0,0 +1,19 @@
+# Ottieni la cartella corrente
+$currentDir = Get-Location
+
+# Estrai l'ultimo segmento del percorso
+$lastSegment = Split-Path $currentDir -Leaf
+
+# Controlla se ti trovi nella cartella 'src'
+if ($lastSegment -ne "src") {
+    # Se non sei in 'src', prova a spostarti in 'src'
+    if (Test-Path ".\src") {
+        Set-Location -Path .\src
+    } else {
+        Write-Output "Cartella 'src' non trovata!"
+        exit
+    }
+}
+
+# Esegui il comando specificato
+& infisical run --env=dev -- go run .
