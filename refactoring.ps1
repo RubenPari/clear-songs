6ed930b05223a0be506eba0e8e98667f04cf0075
RubenPari <rubenpari.developer@gmail.com>
2024-05-27 22:19:08 +0200

    added script for refactoring project

---
+++ refactoring.ps1
@@ -0,0 +1,12 @@
+# Move to the src directory
+cd .\src
+
+# Run gofmt on all Go files in the project
+Get-ChildItem -Recurse -Filter "*.go" | ForEach-Object {
+    gofmt -s -w $_.FullName
+}
+
+# Run goimports on all Go files in the project
+Get-ChildItem -Recurse -Filter "*.go" | ForEach-Object {
+    goimports -w $_.FullName
+}
