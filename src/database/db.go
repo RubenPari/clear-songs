a6584711a162c8f6b953138b023e9d13302274d7
RubenPari <rubenpari.developer@gmail.com>
2024-06-11 11:40:28 +0200

    fixed DB file manager

--- src/database/db.go
+++ src/database/db.go
@@ -3,6 +3,7 @@ package database
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/RubenPari/clear-songs/src/models"
 	_ "github.com/lib/pq"
@@ -10,18 +11,18 @@ import (
 	"gorm.io/gorm"
 )
 
-var db *gorm.DB = nil
+var Db *gorm.DB = nil
 
-func Init() {
+func Init() error {
 	// credential
-	host := "localhost"
-	port := 5432
-	user := "username"
-	password := "password"
-	dbname := "dbname"
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
 
 	// create the connection string
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require",
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
 		host, port, user, password, dbname)
 
 	// Open the connection
@@ -29,30 +30,27 @@ func Init() {
 	db, errConnectDb := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
 
 	if errConnectDb != nil {
-		log.Fatalf("Error connecting to the database: %v", errConnectDb)
+		log.Printf("Error connect database: %v", errConnectDb)
+		return errConnectDb
 	}
 
 	// test connection
 	errTestDb := db.Exec("SELECT 1").Error
 
 	if errTestDb != nil {
-		log.Printf("Error testing the connection to the database: %v", errTestDb)
+		log.Printf("Error test connection: %v", errTestDb)
+		return errTestDb
 	}
 
 	// auto-migration
 	errMigration := db.AutoMigrate(&models.TrackDB{})
 
 	if errMigration != nil {
-		log.Printf("Error migrating the database: %v", errMigration)
+		log.Printf("Error migration: %v", errMigration)
+		return errMigration
 	}
 
-	log.Println("Successfully connected!")
-}
+	log.Println("Successfully connected to database!")
 
-// GetDB returns instances of db
-func GetDB() *gorm.DB {
-	if db == nil {
-		Init()
-	}
-	return db
+	return nil
 }
