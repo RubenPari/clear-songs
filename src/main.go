73a7182674e9112276386182dfead93fca8f74ae
RubenPari <rubenpari.developer@gmail.com>
2024-08-09 00:45:53 +0200

    removed env port value

--- src/main.go
+++ src/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"os"
-
 	"github.com/RubenPari/clear-songs/src/cacheManager"
 	"github.com/RubenPari/clear-songs/src/database"
 	"github.com/RubenPari/clear-songs/src/routes"
@@ -30,7 +28,7 @@ func main() {
 	}
 
 	// start server
-	if server.Run(":"+os.Getenv("PORT")) != nil {
+	if server.Run(":8080") != nil {
 		panic("Error starting server")
 	}
 }
