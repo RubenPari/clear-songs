2868f3fb3666e65a9f2ff9fdaf1ac5fbbe7344e2
RubenPari <rubenpari.developer@gmail.com>
2024-10-18 00:26:36 +0200

    removed some unused constants

--- src/constants/constants.go
+++ src/constants/constants.go
@@ -13,8 +13,4 @@ var Scopes = []string{
 
 const LimitGetPlaylistTracks = 100
 const LimitRemovePlaylistTracks = 100
-const LimitInsertPlaylistTracks = 100
 const Offset = 0
-
-const PlaylistNameWithMinorSongs = "MinorSongs"
-const DescriptionPlaylistNameWithMinorSongs = "A playlist with all tracks from the user library that belong to artists for which the user owns a maximum of 5 tracks."
