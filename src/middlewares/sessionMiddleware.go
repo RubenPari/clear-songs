3001cdc6b98f373cb3271e9a4afc5068dfb531d3
RubenPari <rubenpari.developer@gmail.com>
2025-12-17 01:05:11 +0100

    middleware(session): read OAuth token from Redis-backed cache via typed helper

--- src/middlewares/sessionMiddleware.go
+++ src/middlewares/sessionMiddleware.go
@@ -4,14 +4,12 @@ import (
 	cacheManager "github.com/RubenPari/clear-songs/src/cache"
 	"github.com/RubenPari/clear-songs/src/utils"
 	"github.com/gin-gonic/gin"
-	"golang.org/x/oauth2"
 )
 
 func SessionMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		value := cacheManager.Get("spotify_token")
-		if value != nil {
-			token := value.(*oauth2.Token)
+		token := cacheManager.GetToken()
+		if token != nil {
 			utils.SpotifySvc.SetAccessToken(token)
 			c.Set("spotifyService", utils.SpotifySvc)
 		}
